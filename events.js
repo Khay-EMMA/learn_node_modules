cc457b8caca7df34c12937dfd7f44e75113acd78
njokuScript <kalunjoku123@gmail.com>
2020-03-20 18:01:42 +0100

    worked with event

--- events.js
+++ events.js
@@ -1,10 +1,9 @@
-const EventEmmiter = require("events");
-
-const emmiter = new EventEmmiter();
+const Logger = require("./logger");
+const logger = new Logger();
 //Register an event
-emmiter.on("messageLogged", arg => {
+logger.on("messageLogged", arg => {
   //do something when an event is raised
   console.log("event is logged", arg);
 });
-//Raise an event
-emmiter.emit("messageLogged", { id: 1, url: "https://" });
+
+logger.log("message");
--- logger.js
+++ logger.js
@@ -1,6 +1,12 @@
-let url = "http://logger.io/log";
-log = message => {
-  //send http request
-  console.log(message);
-};
-module.exports = log;
+const EventEmmiter = require("events");
+const url = "https://";
+class Logger extends EventEmmiter {
+  log = message => {
+    //send http request
+    console.log(message);
+    //Raise an event
+    this.emit("messageLogged", { id: 1, url });
+  };
+}
+
+module.exports = Logger;
